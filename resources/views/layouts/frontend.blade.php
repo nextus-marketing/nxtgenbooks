2156c5cef2520ecd827426fc7ae94d301730caa9
Nextus Marketing <marketingteamnextus@gmail.com>
2026-01-07 04:30:05 +0530

    done

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -55,41 +55,6 @@
       <!-- back to top end -->
 
 
-      <!-- search area start -->
-      <div class="search-area">
-         <div class="search-inner p-relative">
-            <div class="container">
-               <div class="row justify-content-center">
-                  <div class="col-lg-8">
-                     <div class="search-wrapper">
-                        <div class="search-close">
-                           <button class="search-close-btn">
-                              <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                 <path d="M11 1L1 11" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                 <path d="M1 1L11 11" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                              </svg>
-                           </button>
-                        </div>
-                        <div class="search-content pt-35">
-                           <h3 class="heading text-center mb-30">Hi! How can we help You?</h3>
-                           <div class="d-flex justify-content-center px-5">
-                              <div class="search w-100 p-relative">
-                                 <input type="text" class="search-input" placeholder="Search...">
-                                 <button  class="search-icon">
-                                 <i class="fa fa-search"></i>
-                                 </button>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
-      <div class="search-overlay"></div>
-      <!-- search area end -->
-
 
       <!-- offcanvas area start -->
       <div class="offcanvas__area">
